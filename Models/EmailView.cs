b7d4ef6a380e67ae851340a11d68ccc0fd797bb5
Massine <116596899+DjoudadMassine@users.noreply.github.com>
2026-05-02 16:17:54 -0400

    well

--- Models/EmailView.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace Models
-{
-    public class EmailView
-    {
-        public string Email { get; set; }
-    }
-}
--- Models/LoginCredential.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Models
-{
-    public class LoginCredential
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public int TimeZoneOffset { get; set; }
-    }
-}
